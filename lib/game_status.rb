d18e7ad7b8c29a90d6de6530d9f0ba2fc9e9d8c8
Matthew Deatherage <mdeatherage1992@gmail.com>
2018-06-11 13:59:18 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -24,14 +24,3 @@ def won?(board)
     end
   end
 end
-
-
-
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
